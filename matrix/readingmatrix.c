c29389c6e225d94076e7f3d3de05da7aa30c9caa
jackrookie-18 <gangkoyhi@gmail.com>
2025-11-18 15:33:27 +0530

    Add matrix programs: addition, reading, and other matrix operations

---
+++ matrix/readingmatrix.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main() {
+    int rows, cols;
+    
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+    
+    int matrix[rows][cols];
+    
+    printf("Enter matrix elements:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+    
+    printf("Matrix elements:\n");
+    for(int i = 0; i < rows; i++) {
+        for(int j = 0; j < cols; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+}
