015ccac3c3bbc06e64ef61f9f29f0a0beb55a48a
jackrookie-18 <gangkoyhi@gmail.com>
2025-12-02 14:51:16 +0530

    Refactor matrix operations: declare loop variables (i, j, k) at start of main()

--- matrix/additionofmatrix.c
+++ matrix/additionofmatrix.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 void main() {
-    int rows, cols;
+    int rows, cols, i, j;
     
     printf("Enter number of rows: ");
     scanf("%d", &rows);
@@ -13,28 +13,28 @@ void main() {
     int result[rows][cols];
     
     printf("Enter elements of first matrix:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
             scanf("%d", &matrix1[i][j]);
         }
     }
     
     printf("Enter elements of second matrix:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
             scanf("%d", &matrix2[i][j]);
         }
     }
     
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
             result[i][j] = matrix1[i][j] + matrix2[i][j];
         }
     }
     
     printf("Resultant matrix after addition:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for (i = 0; i < rows; i++) {
+        for (j = 0; j < cols; j++) {
             printf("%d ", result[i][j]);
         }
         printf("\n");
--- matrix/multiplicationmatrix.c
+++ matrix/multiplicationmatrix.c
@@ -2,7 +2,7 @@
 
 void main()
 {
-    int rows, cols;
+    int rows, cols, i, j, k;
 
     printf("Enter number of rows: ");
     scanf("%d", &rows);
@@ -14,29 +14,29 @@ void main()
     int result[rows][cols];
 
     printf("Enter elements of first matrix:\n");
-    for (int i = 0; i < rows; i++)
+    for (i = 0; i < rows; i++)
     {
-        for (int j = 0; j < cols; j++)
+        for (j = 0; j < cols; j++)
         {
             scanf("%d", &matrix1[i][j]);
         }
     }
 
     printf("Enter elements of second matrix:\n");
-    for (int i = 0; i < rows; i++)
+    for (i = 0; i < rows; i++)
     {
-        for (int j = 0; j < cols; j++)
+        for (j = 0; j < cols; j++)
         {
             scanf("%d", &matrix2[i][j]);
         }
     }
     
-    for (int i = 0; i < rows; i++)
+    for (i = 0; i < rows; i++)
     {
-        for (int j = 0; j < cols; j++)
+        for (j = 0; j < cols; j++)
         {
             result[i][j] = 0;
-            for (int k = 0; k < cols; k++)
+            for (k = 0; k < cols; k++)
             {
                 result[i][j] += matrix1[i][k] * matrix2[k][j];
             }
@@ -44,9 +44,9 @@ void main()
     }
 
     printf("Resultant matrix after multiplication:\n");
-    for (int i = 0; i < rows; i++)
+    for (i = 0; i < rows; i++)
     {
-        for (int j = 0; j < cols; j++)
+        for (j = 0; j < cols; j++)
         {
             printf("%d ", result[i][j]);
         }
